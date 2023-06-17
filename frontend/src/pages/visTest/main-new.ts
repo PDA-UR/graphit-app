9c79e28057ef659501d4e74b2a7c9663e82e1157
Alexander <alexanderweichart@fastmail.com>
2023-06-17 19:06:43 +0200

    fix: new lib changes

--- frontend/src/pages/visTest/main-new.ts
+++ frontend/src/pages/visTest/main-new.ts
@@ -3,14 +3,17 @@ import WikibaseClient from "../../shared/WikibaseClient";
 import { MainViz } from "./vis/MainViz";
 
 async function main() {
-	const wikibase = new WikibaseClient();
+	const credentials = {
+		username: "",
+		password: "",
+	};
+	const wikibase = new WikibaseClient(credentials);
 	const elements = await wikibase.getDependentsAndDependencies();
 	const parents = await wikibase.getCategories();
 
 	const graph = parents.concat(elements);
 
-    const mainViz = new MainViz(graph);
-	
+	const mainViz = new MainViz(graph);
 }
 
 main();
