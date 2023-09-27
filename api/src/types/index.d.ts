3ee86208e7665b7be17694746ba7846653cde253
Alexander <alexanderweichart@fastmail.com>
2023-09-27 12:08:26 +0200

    updated: wb-edit dependency

---
+++ api/src/types/index.d.ts
@@ -0,0 +1,1 @@
+declare module "wikibase-edit-retry";
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,7 +4,6 @@ declare module "cytoscape-expand-collapse" {
 	const ext: cytoscape.Ext;
 	export = ext;
 }
-declare module "wikibase-edit-retry";
 declare module "cytoscape-layout-utilities";
 declare module "cytoscape-cise";
 declare const APP_VERSION: string;
